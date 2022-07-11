88d6529e88e80e5666ac996800950f7dfb11feac
Nando <nando.munalv@gmail.com>
2022-07-11 01:01:36 -0500

    first commit

---
+++ src/main/java/com/nmlv/project/organizedownloadfolder/OrganizeDownloadFolderApplication.java
@@ -0,0 +1,13 @@
+package com.nmlv.project.organizedownloadfolder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizeDownloadFolderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrganizeDownloadFolderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nmlv/project/organizedownloadfolder/OrganizeDownloadFolderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nmlv.project.organizedownloadfolder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganizeDownloadFolderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
